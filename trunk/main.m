2acff7e24a65dec4bcf60b42381291f4ac7bb980
borky89 <borky89@81a65ba2-02a7-11de-b556-9305e745af9e>
2009-02-25 12:41:12 +0000

    initial import

---
+++ trunk/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacDiskTool
+//
+//  Created by Georg Schšlly on 24.02.09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
